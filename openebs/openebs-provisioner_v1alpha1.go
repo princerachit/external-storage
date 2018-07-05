d013b4a6ef879d2a9a362b3e2faf2bbf4f12b043
princerachit <psinha36@gmail.com>
2018-07-05 12:39:49 +0530

    change api calls using gated flags

--- openebs/openebs-provisioner_v1alpha1.go
+++ openebs/openebs-provisioner_v1alpha1.go
@@ -1,58 +1,22 @@
-/*
-Copyright 2017 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
 	"errors"
-	"flag"
 	"fmt"
 	"os"
 	"strings"
 
-	"syscall"
-
 	"github.com/golang/glog"
 	"github.com/kubernetes-incubator/external-storage/lib/controller"
 	"github.com/kubernetes-incubator/external-storage/lib/util"
-	mApiv1 "github.com/kubernetes-incubator/external-storage/openebs/pkg/v1"
+	mApiv1alpha1 "github.com/kubernetes-incubator/external-storage/openebs/pkg/v1alpha1"
 	mayav1 "github.com/kubernetes-incubator/external-storage/openebs/types/v1"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	maya_util "github.com/openebs/maya/pkg/util"
 )
 
-const (
-	provisionerName = "openebs.io/provisioner-iscsi"
-	// BetaStorageClassAnnotation represents the beta/previous StorageClass annotation.
-	// It's currently still used and will be held for backwards compatibility
-	BetaStorageClassAnnotation = "volume.beta.kubernetes.io/storage-class"
-	//defaultFSType
-	defaultFSType = "ext4"
-)
-
-// validFSType represents the valid fstype supported by openebs volume
-// New supported type can be added using OPENEBS_VALID_FSTYPE env
-var validFSType = []string{"ext4", "xfs"}
-
-type openEBSProvisioner struct {
+type openEBSProvisionerV1alpha1 struct {
 	// Maya-API Server URI running in the cluster
 	mapiURI string
 
@@ -62,13 +26,13 @@ type openEBSProvisioner struct {
 }
 
 // NewOpenEBSProvisioner creates a new openebs provisioner
-func NewOpenEBSProvisioner(client kubernetes.Interface) controller.Provisioner {
+func NewOpenEBSProvisionerV1alpha1(client kubernetes.Interface) controller.Provisioner {
 
 	nodeName := os.Getenv("NODE_NAME")
 	if nodeName == "" {
 		glog.Errorf("ENV variable 'NODE_NAME' is not set")
 	}
-	var openebsObj mApiv1.OpenEBSVolume
+	var openebsObj mApiv1alpha1.OpenEBSVolume
 
 	//Get maya-apiserver IP address from cluster
 	addr, err := openebsObj.GetMayaClusterIP(client)
@@ -82,20 +46,18 @@ func NewOpenEBSProvisioner(client kubernetes.Interface) controller.Provisioner {
 	//Set maya-apiserver IP address along with default port
 	os.Setenv("MAPI_ADDR", mayaServiceURI)
 
-	return &openEBSProvisioner{
+	return &openEBSProvisionerV1alpha1{
 		mapiURI:  mayaServiceURI,
 		identity: nodeName,
 	}
 }
 
-var _ controller.Provisioner = &openEBSProvisioner{}
-
 // Provision creates a storage asset and returns a PV object representing it.
-func (p *openEBSProvisioner) Provision(options controller.VolumeOptions) (*v1.PersistentVolume, error) {
+func (p *openEBSProvisionerV1alpha1) Provision(options controller.VolumeOptions) (*v1.PersistentVolume, error) {
 
 	//Issue a request to Maya API Server to create a volume
 	var volume mayav1.Volume
-	var openebsVol mApiv1.OpenEBSVolume
+	var openebsVol mApiv1alpha1.OpenEBSVolume
 	volumeSpec := mayav1.VolumeSpec{}
 
 	volSize := options.PVC.Spec.Resources.Requests[v1.ResourceName(v1.ResourceStorage)]
@@ -209,9 +171,9 @@ func (p *openEBSProvisioner) Provision(options controller.VolumeOptions) (*v1.Pe
 
 // Delete removes the storage asset that was created by Provision represented
 // by the given PV.
-func (p *openEBSProvisioner) Delete(volume *v1.PersistentVolume) error {
+func (p *openEBSProvisionerV1alpha1) Delete(volume *v1.PersistentVolume) error {
 
-	var openebsVol mApiv1.OpenEBSVolume
+	var openebsVol mApiv1alpha1.OpenEBSVolume
 
 	ann, ok := volume.Annotations["openEBSProvisionerIdentity"]
 	if !ok {
@@ -231,116 +193,8 @@ func (p *openEBSProvisioner) Delete(volume *v1.PersistentVolume) error {
 	return nil
 }
 
-func (p *openEBSProvisioner) GetAccessModes() []v1.PersistentVolumeAccessMode {
+func (p *openEBSProvisionerV1alpha1) GetAccessModes() []v1.PersistentVolumeAccessMode {
 	return []v1.PersistentVolumeAccessMode{
 		v1.ReadWriteOnce,
 	}
-}
-
-func main() {
-	var provisioner controller.Provisioner
-	syscall.Umask(0)
-
-	flag.Parse()
-	flag.Set("logtostderr", "true")
-	var (
-		config     *rest.Config
-		err        error
-		k8sMaster  = mayav1.K8sMasterENV()
-		kubeConfig = mayav1.KubeConfigENV()
-	)
-	if len(k8sMaster) != 0 || len(kubeConfig) != 0 {
-		fmt.Printf("Build client config using k8s Master's Address: '%s' or Kubeconfig: '%s' \n", k8sMaster, kubeConfig)
-		config, err = clientcmd.BuildConfigFromFlags(k8sMaster, kubeConfig)
-	} else {
-		// Create an InClusterConfig and use it to create a client for the controller
-		// to use to communicate with Kubernetes
-		config, err = rest.InClusterConfig()
-	}
-	if err != nil {
-		glog.Errorf("Failed to create config: %v", err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		glog.Errorf("Failed to create client: %v", err)
-	}
-
-	// The controller needs to know what the server version is because out-of-tree
-	// provisioners aren't officially supported until 1.5
-	serverVersion, err := clientset.Discovery().ServerVersion()
-	if err != nil {
-		glog.Errorf("Error getting server version: %v", err)
-	}
-
-	// Create the provisioner: it implements the Provisioner interface expected by
-	// the controller.
-	// Create v1alpha1 provisioner if gated feature is on
-	if maya_util.CASTemplateFeatureGate() {
-		glog.Infof("Gated feature is on for v1alpha1")
-		provisioner = NewOpenEBSProvisionerV1alpha1(clientset)
-	} else {
-		provisioner = NewOpenEBSProvisioner(clientset)
-	}
-
-	if provisioner != nil {
-		// Start the provision controller which will dynamically provision OpenEBS VSM
-		// PVs
-		pc := controller.NewProvisionController(
-			clientset,
-			provisionerName,
-			provisioner,
-			serverVersion.GitVersion)
-
-		pc.Run(wait.NeverStop)
-	} else {
-		os.Exit(1) //Exit if provisioner not created.
-	}
-
-}
-
-// GetPersistentVolumeClass returns StorageClassName.
-func GetStorageClassName(options controller.VolumeOptions) *string {
-	// Use beta annotation first
-	if class, found := options.PVC.Annotations[BetaStorageClassAnnotation]; found {
-		return &class
-	}
-	return options.PVC.Spec.StorageClassName
-}
-
-// parseClassParameters extract the new fstype other then "ext4"(dafault) which
-// can be changed via "openebs.io/fstype" key and env OPENEBS_VALID_FSTYPE
-func parseClassParameters(params map[string]string) (string, error) {
-	var fsType string
-	for k, v := range params {
-		switch strings.ToLower(k) {
-		case "openebs.io/fstype":
-			fsType = v
-		}
-	}
-	if len(fsType) == 0 {
-		fsType = defaultFSType
-	}
-
-	//Get openebs supported fstype from ENV variable
-	validENVFSType := os.Getenv("OPENEBS_VALID_FSTYPE")
-	if validENVFSType != "" {
-		slices := strings.Split(validENVFSType, ",")
-		for _, s := range slices {
-			validFSType = append(validFSType, s)
-		}
-	}
-	if !isValid(fsType, validFSType) {
-		return "", fmt.Errorf("Filesystem %s is not supported", fsType)
-	}
-	return fsType, nil
-}
-
-// isValid checks the validity of fstype returns true if supported
-func isValid(value string, list []string) bool {
-	for _, v := range list {
-		if v == value {
-			return true
-		}
-	}
-	return false
 }
